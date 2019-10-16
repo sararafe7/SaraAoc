f68bcc47fbe1b4da686a90635b86a25dc3be2c9e
sararafe7 <sararafe7@gmail.com>
2019-10-16 11:10:19 +0300

    SignUp

--- app/src/main/java/sara/aoc/y2019/MealsActivity.java
+++ app/src/main/java/sara/aoc/y2019/MealsActivity.java
@@ -8,49 +8,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import java.util.Calendar;
 
 public class MealsActivity extends AppCompatActivity {
 
-    @Override
     private Button btnOption;
-   // private void EvantHandler(){
-       // btnOption.setOnClickListener(new View.OnClickListener() {
-         //   @Override
-           // public void onClick(View view) {
-            //    AlertDialog.Builder builder = new AlertDialog.Builder(MealsActivity.this);
-              //  builder.setMessage("Since It Is *hour*, Would You Like to See The *meal* Ingredients?")
-                //        .setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                  //  @Override
-                    //public void onClick(DialogInterface dialogInterface, int which) {
-                      //  MealsActivity
-                    //}
-                //});
 
-            //}
-       // });
-    //}
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meals);
-       // private void EvantHandler() {
-         //   btnOption.setOnClickListener(new View.OnClickListener() {
-           //     @Override
-             //   public void onClick(View view) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MealsActivity.this);
-                    builder= builder.setMessage("Since It Is *hour*, Would You Like to See The *meal* Ingredients?")
-                .setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MealsActivity.this);
+        builder.setMessage("Since It Is"+ HourMinute()+", Would You Like to See The *meal* Ingredients?");
+        builder.setCancelable(false);
+        builder.setNegativeButton("Something Else in mind", null);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int which) {
-                        Intent i = new Intent(this, BreakfastActivity);
-
-                    })
-                       //     .setNegative
+                        Intent i = new Intent(MealsActivity.this, BreakfastActivity.class);
+                        startActivity(i);
+                    }
                 });
-
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
-          //  });
-        //}
-        
+        public static void HourMinute (){
+            Calendar cal = Calendar.getInstance();
+            cal.get(Calendar.HOUR);
+            cal.get(Calendar.MINUTE);
 
+        }
     }
-}
