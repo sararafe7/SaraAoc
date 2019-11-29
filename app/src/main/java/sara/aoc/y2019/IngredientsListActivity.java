649a13d6aba18d8f938b10b43b234514750b85d6
sararafe7 <sararafe7@gmail.com>
2019-11-29 09:13:59 +0200

    SignUp

--- app/src/main/java/sara/aoc/y2019/IngredientsListActivity.java
+++ app/src/main/java/sara/aoc/y2019/IngredientsListActivity.java
@@ -3,12 +3,23 @@ package sara.aoc.y2019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class IngredientsListActivity extends AppCompatActivity {
+
+    ExpandableListView expandableListViewIng;
+    List<String> listGroup;
+    HashMap<String,List<String>>
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredients_list);
+
+        expandableListViewIng = findViewById(R.id.expandableListViewIng);
+        
     }
 }
